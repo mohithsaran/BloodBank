95e57456b0a1a7615e858f878f571e8898bcf96a
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-04-22 17:37:17 +0530

    registeration form is added

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Form from './../../components/Shared/Form/Form';
+
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <>
+      <div className="row">
+        <div className="col-md-8 form-banner">
+          <img src="./assets/images/banner1.jpeg" alt="Login Image"></img>
+        </div>
+        <div className="col-md-4 form-container">
+          <Form formTitle={'Login'} submitBtn={'Login'}/>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login
